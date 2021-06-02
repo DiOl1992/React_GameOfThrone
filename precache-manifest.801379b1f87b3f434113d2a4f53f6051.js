24e7efa2c63f79edab03cbc55e6ca18c1eb921a2
DiOl1992 <57250807+DiOl1992@users.noreply.github.com>
2021-06-02 16:44:32 +0300

    Updates

---
+++ precache-manifest.801379b1f87b3f434113d2a4f53f6051.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0748a76c2d5ecaedab21",
+    "url": "/React_GameOfThrone/static/css/main.d327010d.chunk.css"
+  },
+  {
+    "revision": "0748a76c2d5ecaedab21",
+    "url": "/React_GameOfThrone/static/js/main.0748a76c.chunk.js"
+  },
+  {
+    "revision": "ac2188a37c7b2b409ca4",
+    "url": "/React_GameOfThrone/static/css/1.83874113.chunk.css"
+  },
+  {
+    "revision": "ac2188a37c7b2b409ca4",
+    "url": "/React_GameOfThrone/static/js/1.ac2188a3.chunk.js"
+  },
+  {
+    "revision": "9c73bd7b6bbcdf064f58",
+    "url": "/React_GameOfThrone/static/js/runtime~main.9c73bd7b.js"
+  },
+  {
+    "revision": "9bf7facf7fe7ce064bb69441db3dce17",
+    "url": "/React_GameOfThrone/static/media/error.9bf7facf.jpg"
+  },
+  {
+    "revision": "394af47d0911f37a4ab261162b1bb60f",
+    "url": "/React_GameOfThrone/static/media/got.394af47d.jpg"
+  },
+  {
+    "revision": "a23e6cb2d76fecab16483babadc58af7",
+    "url": "/React_GameOfThrone/index.html"
+  }
+];
